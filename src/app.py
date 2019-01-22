27fdccfb4b7ef3a68781abf25155d16c717e573f
baronkimaru <bizzinali@gmail.com>
2019-01-22 15:36:35 +0300

    initial commit

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
-from flask import Flask,render_template, request, jsonify, session, redirect, url_for
+from flask import Flask,render_template, request, jsonify, session, redirect, url_for, flash
 from pprint import pprint
 import psycopg2
 import psycopg2.extras
 from data import Content
 from wtforms import Form, StringField, PasswordField, validators
 from dbconnect import connect
+from passlib.hash import sha256_crypt
 
 app  = Flask(__name__)
+app.secret_key = b'_5#y2Lddvc23"F4Q8z\n\xec]/'
 
 conn = connect()
 topic_list = Content()
@@ -30,9 +32,11 @@ def article(id):
     return render_template("article.html", id = id)
 
 class RegistrationForm(Form):
-    name = StringField('Name' , [validators.Length(min = 4,max = 20)])
-    username = StringField('Username' , [validators.Length(min = 4,max = 20)])
-    email = StringField('Email' , [validators.Length(min = 6,max = 40)])
+    name = StringField('Name' , [validators.Length(min = 4,max = 20), validators.Required("Please enter your name.")])
+    username = StringField('Username' , [validators.Length(min = 4,max = 20), validators.Required("Please enter your username.")])
+    email = StringField('Email' , [
+        validators.Length(min = 6,max = 40),
+        validators.Email()])
     password = PasswordField('Password' , [
         validators.DataRequired(),
         validators.EqualTo('confirm', message= "Passwords are not The same")
@@ -43,28 +47,31 @@ class RegistrationForm(Form):
 def register():
     """Registers the user drawing from the RegistrationForm class"""
     form  = RegistrationForm(request.form)
+
     try:
         if request.method == "POST" and form.validate():
-            print("Yes!!!")
+            pprint("Posting to Server successful!!!")
             name  = form.name.data
-            username  = form.username.data
             email  = form.email.data
-            password  = form.password.data
+            username  = form.username.data
+            password  = sha256_crypt.encrypt(str(form.password.data))
+            pprint(password)
 
-            print(name)
-            # cur = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
-            # pprint(cur)
-            # sql_insert = """INSERT INTO dev_test.tbl_users(name,email, username, password) VALUES(%s %s %s %s)"""
-            # cur.execute(sql_insert, (name, email, username, password, ))
-            # conn.commit()
-            # cur.close()
-            #
-            # flash("Thank you for registering. Proceed to Login", "success")
-            # redirect(url_for('index'))
+            cur = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
+            if cur:
+                # sql_insert =
+                cur.execute("""INSERT INTO dev_test.tbl_users(name, email, username, password) VALUES(%s, %s, %s, %s)""", (name, email, username, password, ))
+                conn.commit()
+                cur.close()
+
+                flash("Thank you for registering. Proceed to Login", "success")
+                return render_template("index.html")
+
+    except Exception as e:
+        pprint(e),pprint(type(e))
+        pprint("General error")
 
     finally:
-        if conn is not None:
-            conn.close()
         return render_template("register.html" , form = form)
 
 if __name__ == "__main__":
